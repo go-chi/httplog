3645efd41b8299574378691e961509df79660f0f
Peter Kieltyka <peter.kieltyka@gmail.com>
2020-04-01 15:52:19 -0400

    Release v0.1.0

---
+++ _example/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/goware/httplog"
+)
+
+func main() {
+	// Logger
+	logger := httplog.NewLogger("httplog-example", httplog.Options{
+		// JSON:    true,
+		Concise: true,
+		// Tags: map[string]string{
+		// 	"version": "v1.0-81aa4244d9fc8076a",
+		// 	"env":     "dev",
+		// },
+	})
+
+	// Service
+	r := chi.NewRouter()
+	r.Use(httplog.RequestLogger(logger))
+	r.Use(middleware.Heartbeat("/ping"))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("oh no")
+	})
+
+	r.Get("/info", func(w http.ResponseWriter, r *http.Request) {
+		oplog := httplog.LogEntry(r.Context())
+		w.Header().Add("Content-Type", "text/plain")
+		oplog.Info().Msg("info here")
+		w.Write([]byte("info here"))
+	})
+
+	r.Get("/warn", func(w http.ResponseWriter, r *http.Request) {
+		oplog := httplog.LogEntry(r.Context())
+		oplog.Warn().Msg("warn here")
+		w.WriteHeader(400)
+		w.Write([]byte("warn here"))
+	})
+
+	r.Get("/err", func(w http.ResponseWriter, r *http.Request) {
+		oplog := httplog.LogEntry(r.Context())
+		oplog.Error().Msg("err here")
+		w.WriteHeader(500)
+		w.Write([]byte("err here"))
+	})
+
+	http.ListenAndServe(":5555", r)
+}
---
+++ config.go
@@ -0,0 +1,66 @@
+package httplog
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var DefaultOptions = Options{
+	LogLevel: "info",
+	JSON:     false,
+	Concise:  false,
+	Tags:     nil,
+}
+
+type Options struct {
+	// LogLevel defines the minimum level of severity that app should log.
+	//
+	// Must be one of: ["trace", "debug", "info", "warn", "error", "critical"]
+	LogLevel string
+
+	// JSON enables structured logging output in json. Make sure to enable this
+	// in production mode so log aggregators can receive data in parsable format.
+	//
+	// In local development mode, its appropriate to set this value to false to
+	// receive pretty output and stacktraces to stdout.
+	JSON bool
+
+	// Concise mode includes fewer log details during the request flow. For example
+	// exluding details like request content length, user-agent and other details.
+	// This is useful if during development your console is too noisy.
+	Concise bool
+
+	// Tags are additional fields included at the root level of all logs.
+	// These can be useful for example the commit hash of a build, or an environment
+	// name like prod/stg/dev
+	Tags map[string]string
+}
+
+// Configure will set new global/default options for the httplog and behaviour
+// of underlying zerolog pkg and its global logger.
+func Configure(opts Options) {
+	if opts.LogLevel == "" {
+		opts.LogLevel = "info"
+	}
+	DefaultOptions = opts
+
+	// Config the zerolog global logger
+	logLevel, err := zerolog.ParseLevel(opts.LogLevel)
+	if err != nil {
+		fmt.Printf("httplog: error! %v\n", err)
+		os.Exit(1)
+	}
+	zerolog.SetGlobalLevel(logLevel)
+
+	zerolog.LevelFieldName = "severity"
+	zerolog.TimestampFieldName = "timestamp"
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+
+	if !opts.JSON {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+	}
+}
